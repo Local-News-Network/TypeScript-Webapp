49d4662c2d04e69dc3f048548f7d2504bf49f916
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2023-09-17 23:01:29 +0000

    Read More conflict resolved

--- local-news-network/app/ReadMoreButton.tsx
+++ local-news-network/app/ReadMoreButton.tsx
@@ -6,18 +6,12 @@ type Props = {
     article: Article;
 };
 
-function generate(placement) : string {
-    ()
-    return 'bg-orange-400 h-10 rounded-b-lg hover:bg-orange-500'
-
-}
 
 function ReadMoreButton({placement, article} : Props) {
     const router = useRouter();
   return (
     <button
-    className={generate(placement)}
-    >
+    className='bg-orange-400 h-10 rounded-b-lg hover:bg-orange-500'>
      Read More   
     </button>
   )
